9f1c2f49a75a9ef60e8a2a2a74e4b7b80dce1967
YoungBeomLee <edlee127127@gmail.com>
2023-03-20 11:58:38 +0900

    nodemon 다시설치

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const app = express();
 const port = 8080;
 
 const upload = multer({
-	storage: multer.diskStorage({
-		destination: function (req, file, cb) {
-			cb(null, "uploads/");
-		},
-		filename: function (req, file, cb) {
-			cb(null, file.originalname);
-		},
-	}),
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
 });
 
 app.use(express.json());
@@ -22,88 +22,87 @@ app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 app.get("/products", (req, res) => {
-	models.Product.findAll({
-		order: [["createdAt", "DESC"]],
-		attributes: ["id", "name", "price", "seller", "imageUrl", "createdAt"],
-	})
-		.then((result) => {
-			console.log("product 조회결과:", result);
-			res.send({
-				product: result,
-			});
-		})
-		.catch((err) => {
-			console.error(err);
-			res.send("에러발생");
-		});
+  models.Product.findAll({
+    order: [["createdAt", "DESC"]],
+    attributes: ["id", "name", "price", "seller", "imageUrl", "createdAt"],
+  })
+    .then((result) => {
+      console.log("product 조회결과:", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send("에러발생");
+    });
 });
 
 app.get("/products/:id", (req, res) => {
-	const params = req.params;
-	const { id } = params;
-	models.Product.findOne({
-		where: { id: id },
-	})
-		.then((result) => {
-			console.log("조회결과:", result);
-			res.send({
-				product: result,
-			});
-		})
-		.catch((error) => {
-			console.error(error);
-			res.send("상품조회시 에러가 발생했습니다");
-		});
+  const params = req.params;
+  const { id } = params;
+  models.Product.findOne({
+    where: { id: id },
+  })
+    .then((result) => {
+      console.log("조회결과:", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("상품조회시 에러가 발생했습니다");
+    });
 });
 
 app.post("/image", upload.single("image"), (req, res) => {
-	const file = req.file;
-	console.log(file);
-	res.send({
-		imageUrl: file.path,
-	});
+  const file = req.file;
+  res.send({
+    imageUrl: file.path,
+  });
 });
 
 //상품생성데이터를  데이터베이스 추가
 app.post("/products", (req, res) => {
-	const body = req.body;
-	const { name, description, price, seller,imageUrl } = body;
-	if (!name || !description || !price || !seller) {
-		res.send("모든 필드를 입력해주세요");
-	}
-	models.Product.create({
-		name,
-		description,
-		imageUrl,
-		price,
-		seller,
-	})
-		.then((result) => {
-			console.log("상품생성결과:", result);
-			res.send({ result });
-		})
-		.catch((error) => {
-			console.error(error);
-			//res.send("상품업로드에 문제가 발생했습니다");
-		});
+  const body = req.body;
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller) {
+    res.send("모든 필드를 입력해주세요");
+  }
+  models.Product.create({
+    name,
+    description,
+    imageUrl,
+    price,
+    seller,
+  })
+    .then((result) => {
+      console.log("상품생성결과:", result);
+      res.send({ result });
+    })
+    .catch((error) => {
+      console.error(error);
+      //res.send("상품업로드에 문제가 발생했습니다");
+    });
 });
 
 app.post("/login", (req, res) => {
-	res.send("로그인이 완료되었습니다");
+  res.send("로그인이 완료되었습니다");
 });
 
 //app 실행
 app.listen(port, () => {
-	console.log("👩망고샵의 쇼핑몰 서버가 돌아가고 있습니다.🐶멍");
-	//sequelize.sync() DB에 필요한 테이블 생성
-	models.sequelize
-		.sync()
-		.then(() => {
-			console.log("😁DB연결성공");
-		})
-		.catch((err) => {
-			console.error(err);
-			console.log("😨DB연결에러");
-			process.exit();
-		});
+  console.log("👩망고샵의 쇼핑몰 서버가 돌아가고 있습니다.🐶멍");
+  //sequelize.sync() DB에 필요한 테이블 생성
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("😁DB연결성공");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("😨DB연결에러");
+      process.exit();
+    });
 });
